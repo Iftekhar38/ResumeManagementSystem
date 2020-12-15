d483d734ca53294d6697c68c24027a2ed1a911aa
iftekhar <info2iftekhar@gmail.com>
2020-12-15 22:53:43 +0530

    Sending OTP through Email done

--- ResumeMangementSystem/settings.py
+++ ResumeMangementSystem/settings.py
@@ -124,3 +124,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ahmad38.test@gmail.com'
+EMAIL_HOST_PASSWORD = 'iftekhar@1234'
--- process/forms.py
+++ process/forms.py
@@ -1,16 +1,25 @@
 from django import forms
 from process.models import *
 import random
-from process.utils import sendTextMessage
+# from process.utils import sendTextMessage
+from django.conf import settings 
+from django.core.mail import send_mail
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     def clean_otp(self):
-        cno =  self.cleaned_data['contact']
+        email =  self.cleaned_data['email']
         # otp = self.cleaned_data['otp']
         otp = random.randint(100000,999999)
-        message = 'Welcome to RMS and Your OTP is' + str(otp)
-        sendTextMessage(message, cno)
+        touser = email
+        sub = 'OTP'
+        body = 'Welcome to RMS and Your OTP is' + str(otp)
+        efrom = settings.EMAIL_HOST_USER
+        reclist = [touser]
+        send_mail(sub, body, efrom,reclist)
+        
+       # message = 'Welcome to RMS and Your OTP is' + str(otp)
+        #sendTextMessage(message, cno)
         return otp
     
     class Meta:
